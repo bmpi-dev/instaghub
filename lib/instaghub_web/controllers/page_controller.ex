12f8f1f37acd2500a5fa295e5fd8d1d918e6e865
Dawei Ma <madawei2699@gmail.com>
2019-02-27 00:02:42 +0800

    Add homepage html

--- lib/instaghub_web/controllers/page_controller.ex
+++ lib/instaghub_web/controllers/page_controller.ex
@@ -1,7 +1,9 @@
 defmodule InstaghubWeb.PageController do
   use InstaghubWeb, :controller
+  alias Ins.Web.API
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    feeds = API.get_feeds
+    render(conn, "index.html", posts: feeds)
   end
 end

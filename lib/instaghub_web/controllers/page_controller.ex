875e692e2a2b7f4dc19edfb50c28fb88a35d7fbd
Dawei Ma <madawei2699@gmail.com>
2019-03-01 00:43:06 +0800

    Add cache plug

--- lib/instaghub_web/controllers/page_controller.ex
+++ lib/instaghub_web/controllers/page_controller.ex
@@ -2,28 +2,22 @@ defmodule InstaghubWeb.PageController do
   use InstaghubWeb, :controller
   alias Ins.Web.API
   alias Instaghub.RedisUtil
-  alias Instaghub.Utils
+  alias InstaghubWeb.Plug.Cache
   require Logger
 
-  def index(conn, params) do
-    cursor = Map.get(params, "cursor")
-    redis_key = if cursor == nil do
-      "index"
-    else
-      "index" <> cursor
-    end
-    redis_key_md5 = Utils.md5_base64(redis_key)
-    value = RedisUtil.get(redis_key_md5)
+  def index(conn, _params) do
+    cursor = Cache.get_cursor(conn)
+    redis_key_md5 = Cache.get_page_key(conn, cursor)
+    page = Cache.get_page(conn)
     feeds_with_page =
-    if value == nil do
+    if page == nil do
       feeds_with_page = API.get_feeds(cursor)
       feeds_bin   = :erlang.term_to_binary(feeds_with_page)
       RedisUtil.setx(redis_key_md5, feeds_bin)
       Logger.debug "get page with api and store in redis with key #{redis_key_md5}"
       feeds_with_page
     else
-      Logger.debug "get page with redis key #{redis_key_md5}"
-      :erlang.binary_to_term value
+      page
     end
     render(conn, "index.html", posts: feeds_with_page.posts, page_info: feeds_with_page.page_info)
   end
--- lib/instaghub_web/router.ex
+++ lib/instaghub_web/router.ex
@@ -1,5 +1,6 @@
 defmodule InstaghubWeb.Router do
   use InstaghubWeb, :router
+  alias InstaghubWeb.Plug.Cache
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -7,6 +8,7 @@ defmodule InstaghubWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Cache, []
   end
 
   pipeline :api do

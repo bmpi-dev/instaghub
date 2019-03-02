d929cc1ee3525ddb5e0bd96024866c55665889c9
Dawei Ma <madawei2699@gmail.com>
2019-03-03 00:58:22 +0800

    Parse caption and comments user link and hashtag link

--- lib/instaghub/utils.ex
+++ lib/instaghub/utils.ex
@@ -1,6 +1,27 @@
 defmodule Instaghub.Utils do
+  alias Phoenix.HTML
+
   def md5_base64(str) do
     :crypto.hash(:md5, str)
     |> Base.encode64()
+  end
+
+  def parse_link(str) do
+    str
+    |> parse_user_link
+    |> parse_hashtag_link
+    |> HTML.raw
+  end
+
+  defp parse_user_link(str) do
+    str
+    |> (fn(x) -> Regex.replace(~r/[@][^\s#@$!?]+/, x, "<a href=\"/user/\\0\">\\0</a>", global: true) end).()
+    |> String.replace("/user/@", "/user/")
+  end
+
+  defp parse_hashtag_link(str) do
+    str
+    |> (fn(x) -> Regex.replace(~r/[#][^\s#@$!?]+/, x, "<a href=\"/tag/\\0\">\\0</a>", global: true) end).()
+    |> String.replace("/tag/#", "/tag/")
   end
 end

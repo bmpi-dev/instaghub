c0c2efeace19dacf42b1ec491bbb5bac28a94168
Dawei Ma <madawei2699@gmail.com>
2019-03-03 22:25:01 +0800

    Add jquery, masonry, imagesLoaded to layout home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import css from "../css/app.css";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -9,44 +9,64 @@ import css from "../css/app.css"
 //
 // Import dependencies
 //
-import "phoenix_html"
+import "phoenix_html";
+var Masonry = require('masonry-layout');
+var Imagesloaded = require('imagesloaded');
+var jQueryBridget = require('jquery-bridget');
 // make $ available in Chrome console
-window.$ = window.jQuery = require("jquery")
+$ = window.$ = window.jQuery = require("jquery");
+// make Masonry a jQuery plugin
+jQueryBridget( 'masonry', Masonry, $ );
+// provide jQuery argument
+Imagesloaded.makeJQueryPlugin( $ );
+
+var $grid = $('.grid').masonry({
+    // options
+    itemSelector: '.grid-item',
+    //horizontalOrder: true,
+    //isFitWidth: true
+});
+
+// init layout masonry
+$grid;
 
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
-var is_get_new_page = 0
+var is_get_new_page = 0;
 $(window).scroll(function() {
     if($(window).scrollTop() + $(window).height() == $(document).height()) {
         // bottom to get new page
         if(is_get_new_page == 0) {
-            $("#load_next_page").show()
-            is_get_new_page = 1
-            var page_len = $(".page").length
-            var cursor = $(".page")[page_len - 1].textContent
+            $("#load_next_page").show();
+            is_get_new_page = 1;
+            var cursor = $(".page")[0].textContent;
             $.ajax({
                 url: "/?cursor=" + cursor,
                 success: function(res) {
-                    var new_page = res
-                    var pages = $(".page")
-                    var is_append = true
-                    for(var i=0; i<pages.length; i++) {
-                        var res_dom = $(res)
-                        if(pages[i].textContent === $('.page', res_dom)[0].textContent){
-                            is_append = false
-                            console.log("find page, break")
-                            break
-                        }
-                    }
-                    if(is_append) {
-                        $(".grid").append(res)
-                    }
-                    is_get_new_page = 0
-                    $("#load_next_page").hide()
+                    var $res = $(res);
+                    var $page = $res.filter('.page')[0];
+                    $(".page")[0].textContent = $page.textContent;
+                    $grid.masonryImagesReveal($res);
                 }
-            })
+            });
         }
     }
 });
+
+$.fn.masonryImagesReveal = function($items) {
+    // hide by default
+    $items.hide();
+    // append to container
+    $(".grid").append($items);
+    $items.imagesLoaded().progress(function(imgLoad, image) {
+        // image is imagesLoaded class, not <img>, <img> is image.img
+        var $item = $( image.img ).parents(".grid-item");
+        $item.show();
+        $grid.masonry('appended', $item);
+        is_get_new_page = 0;
+        $("#load_next_page").hide();
+    });
+    return this;
+};
